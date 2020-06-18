2636ef37046e55a7113a3f3b54a7158d8ba9402d
leon20121005 <leon20121005@gmail.com>
2020-06-18 18:05:35 +0800

    refactoring, filter the waveform, determine the best number of clusters and write the corresponding result files

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import csv
 import os
+import math
 import matplotlib.pyplot as plot
 import statistics
+import time
 from sklearn.metrics import mean_squared_error
 from waveform_slicer import WaveformSlicer
 
-ROOT_PATHS = ['datasets/1000/', 'datasets/1015/']
+STATISTICS_FILE_HEADER = ['folder_name', 'L/R', 'age', 'gender', 'number_of_waves',
+                          'valleys_distance_avg', 'valleys_distance_std', 'valleys_distance_from_heartbeat', 'valleys_distance_from_pulse',
+                          'difference_between_heartbeat_and_average(mse)', 'difference_between_heartbeat_and_average(mse_sqrt)', 'difference_between_heartbeat_and_average(me)',
+                          'difference_between_pulse_and_average(mse)', 'difference_between_pulse_and_average(mse_sqrt)', 'difference_between_pulse_and_average(me)']
+RESULT_FILE_HEADER = ['folder_name', 'number_of_waves', 'valleys_x']
+
+# ROOT_PATHS = ['datasets/1000/', 'datasets/1015/', 'datasets/Archive/']
+ROOT_PATHS = ['datasets/Archive/']
+
+START_TIME = int(time.time())
+
+STOPPING_LIST = []
+
+BIAS = 1500
+
+IS_FIGURE_SHOWN = False
 
 def read_directories(root_path):
     for root, directories, file_names in os.walk(root_path):
@@ -30,30 +47,58 @@ def read_samples(directories):
     return samples
 
 
-def plot_time_domain_amplitude(data, peaks, slicing_peaks):
+def read_information(dataset_path):
+    information = []
+    try:
+        rows = read_csv_file(dataset_path + '001full.csv')
+        id = rows[0][0]
+        gender = rows[0][1]
+        age = rows[0][2]
+        heartbeat = int(rows[0][3].split('/')[1].split(' ')[1])
+        if '-R' in id:
+            information.append('R')
+        else:
+            information.append('L')
+        information.append(age)
+        information.append(gender)
+        information.append(heartbeat)
+        pulse_frequencies = []
+        for index in range(4):
+            rows = read_csv_file(dataset_path + f'00{str(index + 1)}full.csv')
+            pulse_frequencies.append(float(rows[1][10]))
+        information.append(statistics.mean(pulse_frequencies))
+    except:
+        information = None
+    return information
+
+
+def plot_time_domain_amplitude(data, peaks, slicing_peaks, patient_information, is_figure_shown):
+    patient_information_text = 'folder_ID: {}\nposition: {}\ngender: {}\nage: {}\nheartbeat: {}, pulse: {}'.format(*patient_information)
     plot.xlabel('Time')
     plot.ylabel('Amplitude')
     plot.plot([x for x, y in data], [y for x, y in data], color = 'cornflowerblue')      # plot original data
     plot.scatter([x for x, y in peaks], [y for x, y in peaks], color = 'mediumseagreen') # plot peaks
     for slicing_peak in slicing_peaks:
         plot.axvline(x = slicing_peak[0], color = 'grey', linestyle = '--')              # plot slicing lines
-    plot.show()
+    plot.text(0.8, 0.9, patient_information_text, fontsize = 9, transform = plot.gcf().transFigure)
+    if is_figure_shown is True:
+        plot.show()
+    else:
+        plot.gcf().set_size_inches(16, 9)
+        plot.savefig(f'results_{START_TIME}/{patient_information[0]}.jpg')
+        plot.close()
     return
 
 
-def read_heartbeat(dataset_name):
-    file = open(dataset_name + '001full.csv', 'r', encoding = 'utf-8')
+def read_csv_file(file_path):
+    file = open(file_path, 'r', encoding = 'utf-8')
     rows = list(csv.reader(file))
     file.close()
-    try:
-        heartbeat = int(rows[0][3].split('/')[1].split(' ')[1])
-    except:
-        heartbeat = None
-    return heartbeat
+    return rows
 
 
-def write_file(file, dataset_name, wave_lengths_mean, wave_lengths_std, standard_wave_length, loss, loss_2):
-    file.write('{}, {}, {}, {}, {}, {}\n'.format(dataset_name, wave_lengths_mean, wave_lengths_std, standard_wave_length, loss, loss_2))
+def write_csv_file(file, values):
+    file.write(','.join([str(value) for value in values]) + '\n')
     return
 
 
@@ -64,24 +109,35 @@ if __name__ == '__main__':
         directories += read_directories(root_path)
     samples = read_samples(directories)
 
-    output_file = open('result.csv', 'w', encoding = 'utf_8_sig')
+    os.mkdir(f'results_{START_TIME}')
+    statistics_file = open(f'results_{START_TIME}/result.csv', 'w', encoding = 'utf_8_sig')
+    result_file = open(f'results_{START_TIME}/result2.csv', 'w', encoding = 'utf_8_sig')
+
+    write_csv_file(statistics_file, STATISTICS_FILE_HEADER)
+    write_csv_file(result_file, RESULT_FILE_HEADER)
 
     for sample_index, sample in enumerate(samples):
-        dataset_name = directories[sample_index]
-        print(dataset_name)
+        dataset_path = directories[sample_index]
+        dataset_name = dataset_path.split('/')[2]
+        print(dataset_path)
+
+        if len(STOPPING_LIST) > 0 and dataset_name not in STOPPING_LIST:
+            continue
 
         if len(sample) == 0:
             print('skipped')
             continue
 
-        sample = sample[1500:]
+        sample = sample[BIAS:]
 
-        # heartbeat
-        heartbeat = read_heartbeat(dataset_name)
-        if heartbeat is None:
+        # information
+        information = read_information(dataset_path)
+        if information is None:
             print('skipped')
             continue
-        standard_wave_length = heartbeat / 60 * 500
+        hand, age, gender, heartbeat, pulse_frequency = information
+        standard_wave_length = 60 / heartbeat * 500
+        standard_pulse_wave_length = 1 / pulse_frequency * 500
 
         # data: [(index, amplitude), ...]
         slicer = WaveformSlicer()
@@ -89,24 +145,32 @@ if __name__ == '__main__':
         peaks = slicer.get_peaks()
         slicing_peaks = slicer.get_slicing_peaks()
 
-        # plot_time_domain_amplitude(data, peaks, slicing_peaks)
+        patient_information = dataset_name, hand, gender, age, heartbeat, round(pulse_frequency * 60)
+        plot_time_domain_amplitude(sample, peaks, slicing_peaks, patient_information, IS_FIGURE_SHOWN)
 
         wave_lengths = []
         for index in range(len(slicing_peaks) - 1):
             wave_lengths.append(slicing_peaks[index + 1][0] - slicing_peaks[index][0])
 
+        number_of_waves = len(wave_lengths)
         wave_lengths_mean = statistics.mean(wave_lengths)
         wave_lengths_std = statistics.stdev(wave_lengths)
-        standard_wave_lengths = [standard_wave_length] * len(wave_lengths)
-        loss = mean_squared_error(standard_wave_lengths, wave_lengths)
-        loss_2 = (standard_wave_length - sum(wave_lengths) / len(wave_lengths)) / len(wave_lengths)
+        standard_wave_lengths = [standard_wave_length] * number_of_waves
+        standard_pulse_wave_lengths = [standard_pulse_wave_length] * number_of_waves
+        loss_mse = mean_squared_error(standard_wave_lengths, wave_lengths)
+        loss_mse_pulse = mean_squared_error(standard_pulse_wave_lengths, wave_lengths)
+        loss_mse_sqrt = math.sqrt(loss_mse)
+        loss_mse_sqrt_pulse = math.sqrt(loss_mse_pulse)
+        loss_me = (standard_wave_length - sum(wave_lengths) / number_of_waves) / number_of_waves
+        loss_me_pulse = (standard_pulse_wave_length - sum(wave_lengths) / number_of_waves) / number_of_waves
 
-        print(wave_lengths)
-        print('Mean:', wave_lengths_mean)
-        print('Std:', wave_lengths_std)
-        print('Heartbeat length:', standard_wave_length)
-        print('Loss (MSE):', loss)
-        print('Loss (ME):', loss_2)
+        slicing_peak_indexes = [str(slicing_peak[0] - BIAS) for slicing_peak in slicing_peaks]
+        slicing_peak_indexes = ', '.join(slicing_peak_indexes)
 
-        write_file(output_file, dataset_name.split('/')[2], wave_lengths_mean, wave_lengths_std, standard_wave_length, loss, loss_2)
-    output_file.close()
+        write_csv_file(statistics_file, [dataset_name, hand, age, gender, number_of_waves,
+                                         round(wave_lengths_mean), round(wave_lengths_std), round(standard_wave_length), round(standard_pulse_wave_length),
+                                         round(loss_mse), round(loss_mse_sqrt), round(loss_me),
+                                         round(loss_mse_pulse), round(loss_mse_sqrt_pulse), round(loss_me_pulse)])
+        write_csv_file(result_file, [dataset_name, len(slicing_peaks) - 1, f'"{slicing_peak_indexes}"'])
+    statistics_file.close()
+    result_file.close()
--- waveform_slicer.py
+++ waveform_slicer.py
@@ -1,4 +1,7 @@
+from scipy import signal
 from sklearn import cluster
+
+IS_VERBOSE = True
 
 class WaveformSlicer:
 
@@ -6,91 +9,126 @@ class WaveformSlicer:
         return
 
 
+    def _find_filtered_data(self, data):
+        indexes = [x for x, y in data]
+        amplitudes = [y for x, y in data]
+        b, a = signal.ellip(4, 0.01, 120, 0.125)
+        filtered_amplitudes = signal.filtfilt(b, a, amplitudes, padlen = 50)
+        filtered_data = [(index, filtered_amplitude) for index, filtered_amplitude in zip(indexes, filtered_amplitudes)]
+        return filtered_data
+
+
     # 找出所有轉折點
-    def _find_peaks(self):
+    def _find_peaks(self, data):
         peaks = []
         previous_slope = None
-        for index in range(1, len(self.data)):
-            difference = self.data[index][1] - self.data[index - 1][1]
+        for index in range(1, len(data)):
+            difference = data[index][1] - data[index - 1][1]
             if difference == 0:
                 continue
             if difference > 0:
                 if previous_slope is 'negative':
-                    peaks.append(self.data[index - 1])
+                    peaks.append(data[index - 1])
                 previous_slope = 'positive'
                 continue
             if difference < 0:
                 if previous_slope is 'positive':
-                    peaks.append(self.data[index - 1])
+                    peaks.append(data[index - 1])
                 previous_slope = 'negative'
                 continue
         return peaks
 
 
+    def _find_best_cluster_labels_and_centers(self, peaks):
+        results = []
+        for k in [2, 3]:
+            kmeans = cluster.KMeans(n_clusters = k).fit([(0, peak[1]) for peak in peaks])
+            cluster_labels = kmeans.labels_           # [label of peak, ...]
+            cluster_centers = kmeans.cluster_centers_ # [[center of x in cluster 1, center of y in cluster 1], ...]
+
+            centers = [cluster_centers[cluster_label][1] for cluster_label in cluster_labels]
+            loss = 0
+            for peak, center in zip(peaks, centers):
+                loss += abs(peak[1] - center)
+            results.append([cluster_labels, cluster_centers, loss])
+
+            self._log(message = f'Loss (k = {k}): {loss}')
+
+        if results[0][2] < 2 * results[1][2]: # loss(k = 2) < 2 x loss(k = 3)
+            self._log(message = 'Selected k: 2')
+            return (results[0][0], results[0][1])
+        else:
+            self._log(message = 'Selected k: 3')
+            return (results[1][0], results[1][1])
+
+
+    def _log(self, message):
+        if IS_VERBOSE is True:
+            print(message)
+        return
+
+
     # 找出cluster_center最大的label(波峰的label)
-    def _find_largest_cluster_label(self):
-        largest_cluster = 0
-        largest_center = 0
-        for index in range(len(self.cluster_centers)):
-            if self.cluster_centers[index][1] > largest_center:
-                largest_cluster = index
-                largest_center = self.cluster_centers[index][1]
-        return largest_cluster
+    def _find_largest_cluster_label(self, cluster_centers):
+        largest_cluster_label = 0
+        largest_center = cluster_centers[0][1]
+        for index in range(1, len(cluster_centers)):
+            if cluster_centers[index][1] > largest_center:
+                largest_cluster_label = index
+                largest_center = cluster_centers[index][1]
+        return largest_cluster_label
 
 
-    # 找出波谷的index
-    def _find_wave_trough_indexes(self):
+    # 找出cluster_center最小的label(波谷的label)
+    def _find_smallest_cluster_label(self, cluster_centers):
+        smallest_cluster_label = 0
+        smallest_center = cluster_centers[0][1]
+        for index in range(1, len(cluster_centers)):
+            if cluster_centers[index][1] < smallest_center:
+                smallest_cluster_label = index
+                smallest_center = cluster_centers[index][1]
+        return smallest_cluster_label
+
+
+    # 找出波谷的index(基於cluster_labels)
+    def _find_wave_trough_indexes(self, cluster_labels, wave_crest_label, wave_trough_label):
         wave_trough_indexes = []
-        for index in range(1, len(self.cluster_labels)):
-            if self.cluster_labels[index - 1] != self.wave_crest_label and self.cluster_labels[index] == self.wave_crest_label:
+        for index in range(1, len(cluster_labels)):
+            if cluster_labels[index - 1] == wave_trough_label and cluster_labels[index] == wave_crest_label:
                 wave_trough_indexes.append(index - 1)
         return wave_trough_indexes
 
 
-    # 找出切割波長的轉折點的index
-    def _find_slicing_peak_indexes(self):
-        slicing_peak_indexes = []
-        for index in range(1, len(self.cluster_labels)):
-            if self.cluster_labels[index - 1] == self.wave_crest_label and self.cluster_labels[index] != self.wave_crest_label:
-                slicing_peak_indexes.append(index)
-        return slicing_peak_indexes
-
-
     # 找出切割波長的轉折點
-    def _find_slicing_peaks(self):
+    def _find_slicing_peaks(self, peaks, slicing_peak_indexes):
         slicing_peaks = []
-        for index in range(len(self.peaks)):
-            if index in self.slicing_peak_indexes:
-                slicing_peaks.append(self.peaks[index])
+        for index in range(len(peaks)):
+            if index in slicing_peak_indexes:
+                slicing_peaks.append(peaks[index])
         return slicing_peaks
 
 
     # 根據切割波長的轉折點來切割data
-    def _slice_data(self):
+    def _slice_data(self, data, slicing_peaks):
         waves = []
-        slicing_peaks_x = [slicing_peak[0] for slicing_peak in self.slicing_peaks]
+        slicing_peaks_x = [slicing_peak[0] for slicing_peak in slicing_peaks]
         slicing_peaks_x = [-1] + slicing_peaks_x
         for index in range(len(slicing_peaks_x) - 1):
-            waves.append(self.data[slicing_peaks_x[index] + 1:slicing_peaks_x[index + 1]])
+            waves.append(data[slicing_peaks_x[index] + 1:slicing_peaks_x[index + 1]])
         return waves
 
 
     def fit(self, data):
-        # data: [(index, amplitude), ...]
-        self.data = data
+        self.data = data                                                # data:          [(index, amplitude), ...]
+        self.filtered_data = self._find_filtered_data(data = self.data) # filtered_data: [(index, amplitude), ...]
+        self.peaks = self._find_peaks(data = self.filtered_data)        # peaks:         [(index, amplitude), ...]
 
-        # peaks: [(index, amplitude), ...]
-        self.peaks = self._find_peaks()
-
-        kmeans = cluster.KMeans(n_clusters = 3).fit([(0, peak[1]) for peak in self.peaks])
-        # cluster_labels: [label of peak, ...]
-        self.cluster_labels = kmeans.labels_
-        self.cluster_centers = kmeans.cluster_centers_
-
-        self.wave_crest_label = self._find_largest_cluster_label()
-        self.slicing_peak_indexes = self._find_wave_trough_indexes()
-        self.slicing_peaks = self._find_slicing_peaks()
-        self.waves = self._slice_data()
+        self.cluster_labels, self.cluster_centers = self._find_best_cluster_labels_and_centers(peaks = self.peaks)
+        self.wave_crest_label = self._find_largest_cluster_label(cluster_centers = self.cluster_centers)
+        self.wave_trough_label = self._find_smallest_cluster_label(cluster_centers = self.cluster_centers)
+        self.slicing_peak_indexes = self._find_wave_trough_indexes(cluster_labels = self.cluster_labels, wave_crest_label = self.wave_crest_label, wave_trough_label = self.wave_trough_label)
+        self.slicing_peaks = self._find_slicing_peaks(peaks = self.peaks, slicing_peak_indexes = self.slicing_peak_indexes)
+        self.waves = self._slice_data(data = self.data, slicing_peaks = self.slicing_peaks)
         return
 
 
